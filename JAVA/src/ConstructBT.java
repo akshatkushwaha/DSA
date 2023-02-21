88963877cdc3a2cd3890416405a15caeb4ef7a9a
Akshat Kushwaha <akshat.official999@gmail.com>
2023-02-22 01:35:00 +0530

    ConstructBT using Preorder and Inorder traversal Array

--- JAVA/src/ConstructBT.java
+++ JAVA/src/ConstructBT.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+
 public class ConstructBT {
     public static void main(String[] args) {
 
     }
 
     public static TreeNode buildTreePreorderInorder(int[] preorder, int[] inorder){
+        if(preorder.length == 0)
+            return null;
+
         int current = preorder[0];
         TreeNode root = new TreeNode(current);
 
@@ -21,7 +26,15 @@ public class ConstructBT {
         int[] rightPreorder = new int[preorder.length - position - 1];
         int[] rightInorder = new int[preorder.length - position - 1];
 
+        System.arraycopy(inorder, 0, leftInorder, 0, position);
+        System.arraycopy(inorder, position + 1, rightInorder, 0, rightInorder.length);
+        System.arraycopy(preorder, 1, leftPreorder, 0, position);
+        System.arraycopy(preorder, position + 1, rightPreorder, 0, rightPreorder.length);
 
+        System.out.println(Arrays.toString(leftPreorder));
+        System.out.println(Arrays.toString(leftInorder));
+        System.out.println(Arrays.toString(rightPreorder));
+        System.out.println(Arrays.toString(rightInorder));
 
         root.left = buildTreePreorderInorder(leftPreorder, leftInorder);
         root.right = buildTreePreorderInorder(rightPreorder, rightInorder);
